c837b26106a1ad0b09078c1ab22fe89d40334396
Shayne Hewitt <shewitt565@gmail.com>
2025-04-27 18:08:01 -0400

    Runs, returns Hello World page

--- app.py
+++ app.py
@@ -5,8 +5,23 @@ app = Flask(__name__)
 # TODO: create 404 template
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template(), 404
+    return render_template('404.html'), 404
+
+'''
+@app.errorhandler(500)
+def page_unavailable(error):
+    return render_template('500.html'), 500
+'''
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+# @app.route('/')
+# def ():
+
+# DEBUG!!
+'''
+if __name__ == "__main__":
+    app.run(debug=True)
+'''
