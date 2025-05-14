0636a15490c5ee3d1170bff67e99b3e9c0c84475
Shayne Hewitt <shewitt565@gmail.com>
2025-05-13 22:29:28 -0400

    Updated about page. Tweaked CSS configs

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, abort, request, url_for, flash, redire
 
 app = Flask(__name__)
 
-# TODO: create 404 template
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
@@ -29,5 +28,4 @@ def products():
 TODO: 
 - Facebook fetcher
 - Add favicon https://www.w3schools.com/howto/howto_html_favicon.asp#:~:text=A%20favicon%20is%20a%20small,simple%20image%20with%20high%20contrast.
-- Refine pages
 """
