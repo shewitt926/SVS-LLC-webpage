9eea6c5e13a418740e09d2dff2456a7caf23dcf3
Shayne Hewitt <shewitt565@gmail.com>
2025-05-18 23:09:18 -0400

    Modified more login stuff

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Scott's Variety Shop Webpage
 A web app that is Scott's Variety Shop LLC's storefront. 
 
+## Background
 Since the shutdown of the eBay store under the name luvinthetaco, we aim to create a new and custom storefront to update customers on any news, new items, and anything else that comes to be. 
 
-The source code will be written in Python--built using Flask--and there will be plans to add other features, but they are not ready for deployment yet.
+The source code will be written in Python--built using Flask--and there will be plans to add more than basic webstore features, but they are not ready for deployment yet. 
+
+## Features
+Current features that will be implemented are:
+- Direct integration from our Facebook page
+- Products page with checkout options
 
 # THIS IS A WORK IN PROGRESS, SO PLEASE ALLOW US TIME TO IMPROVE
--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 from flask import Flask, render_template, abort, request, url_for, flash, redirect
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Needed for session management
 
-'''
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+# Dummy user store
+users = {'admin': {'password': 'password123'}}
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id) # Fetch user from database
-'''
+    if user_id in users:
+        return User(user_id)
+    return None
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
-
-'''
-@app.errorhandler(500)
-def page_unavailable(error):
-    return render_template('500.html'), 500
-'''
-
+# wanna see the webpage?
 @app.route('/')
 def homepage():
     return render_template('home.html')
@@ -35,9 +34,38 @@ def about():
 def products():
     return render_template('products.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['Username']
+        password = request.form['Password']
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('homepage'))
+        else:
+            flash('Invalid credentials, try again!')
     return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
+
+@app.route('/protected')
+@login_required
+def protected():
+    return f'Hello, {current_user.id}! This is a protected page.'
+
+# error handling, yeah!
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_unavailable(error):
+    return render_template('500.html'), 500
 
 """ 
 TODO: 
